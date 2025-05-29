517798f1e0b2727079264662ebbbb064aec81f25
nelsonchg <nelsonchg2@gmail.com>
2025-05-29 17:17:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
-from flask import Flask, jsonify, send_file
+from flask import Flask
+import parser
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Привет! Python-проект работает на Render."
-
-@app.route('/reviews')
-def get_reviews():
-    try:
-        with open('reviews.json', 'r', encoding='utf-8') as f:
-            import json
-            data = json.load(f)
-        return jsonify(data)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-@app.route('/html')
-def get_html():
-    return send_file("index.html")
+def index():
+    parser.main()
+    return 'parser.py запущен!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run()
